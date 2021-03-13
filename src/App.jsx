cf42628136aafdfb2baa3b9f13414e8fd50ef538
Baal <baal.wu@bridgewell.com>
2021-03-13 08:26:32 +0800

    [feature] fix ios scroll fixed element bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
 import { Navbar } from './components/nav-bar'
 import { Content } from './components/content'
 
-const App = () => (
-  <BrowserRouter>
-    <div className="w-screen h-screen overflow-x-hidden">
-      <Navbar />
-      <Content />
-    </div>
-  </BrowserRouter>
-)
+const App = () => {
+  const navbarRef = useRef()
+  useEffect(() => {
+    const navbarSection = navbarRef.current
+    if (navbarSection) {
+      navbarSection.addEventListener('touchmove', (e) => {
+        e.preventDefault()
+      })
+    }
+  }, [])
+  return (
+    <BrowserRouter>
+      <div ref={navbarRef} className="w-screen h-screen overflow-x-hidden">
+        <Navbar />
+        <Content />
+      </div>
+    </BrowserRouter>
+  )
+}
 
 export default App
